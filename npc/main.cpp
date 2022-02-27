b0a82b6847fb87155031bcd1a475fd8a2e07a304
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-27 16:22:45 +0800

    >  sim RTL ysyx_22040386 程钇力 Linux cyl-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:22:45 up  4:17,  3 users,  load average: 0.13, 0.06, 0.01

---
+++ npc/main.cpp
@@ -0,0 +1,23 @@
+#include "Vexample.h"
+#include "verilated.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main(int argc, char** argv, char** env) {
+	VerilatedContext* contextp = new VerilatedContext;
+	contextp->commandArgs(argc, argv);
+	Vexample* top = new Vexample{contextp};
+	while (!contextp->gotFinish()) {
+		int a = rand() & 1;
+		int b = rand() & 1;
+		top->a = a;
+		top->b = b;
+		top->eval();
+		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+		assert(top->f == a ^ b);
+	}
+	delete top;
+	delete contextp;
+	return 0;
+}
