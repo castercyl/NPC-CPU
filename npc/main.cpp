d2db7abc5e548346ae8ea51163659f82ba2ff62d
tracer-ysyx2204 <tracer@ysyx.org>
2022-02-28 14:59:42 +0800

    >  sim RTL ysyx_22040386 程钇力 Linux cyl-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:59:42 up  1:38,  5 users,  load average: 0.02, 0.03, 0.00

--- npc/main.cpp
+++ npc/main.cpp
@@ -1,5 +1,5 @@
 #include "verilated_vcd_c.h"
-#include "Vexample.h"
+#include "Vwaterlight.h"
 #include "verilated.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,25 +7,36 @@
 
 vluint64_t main_time = 0; // 仿真初始时间为0 
 
+void single_cycle() {
+	  top->clk = 0; top->eval();
+	    top->clk = 1; top->eval();
+}
+
+void rst(int n) {
+	  top->reset = 1;
+	    while (n -- > 0) single_cycle();
+		  top->reset = 0;
+}
 
 int main(int argc, char** argv, char** env) {
 	VerilatedContext* contextp = new VerilatedContext;
 	contextp->commandArgs(argc, argv);
-	Verilated::traceEverOn(true);
+	Verilated::traceEverOn(true); //打开波形追踪
 	VerilatedVcdC* tfp = new VerilatedVcdC;
-	Vexample* top = new Vexample{contextp};
+	Vwaterlight* top = new Vwaterlight{contextp};
 	top->trace(tfp, 99);
-	tfp->open("wave.vcd");
+	tfp->open("waterlight-wave.vcd");
+
+	rst(10);                     //复位10个周期
+
 	while (!contextp->gotFinish()) {
 		contextp->timeInc(1);
-		int a = rand() & 1;
-		int b = rand() & 1;
-		top->a = a;
-		top->b = b;
+		
+		single-cycle();
+
 		top->eval();
 		tfp->dump(main_time);   // 波形文件写入步进
-		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-		assert(top->f == a ^ b);
+		printf("led = %d\n", top->led);
 		main_time++;
 	}
 	delete top;
---
+++ npc/waterlight.v
@@ -0,0 +1,19 @@
+module light(
+	input reset,
+	input clk,
+	output reg [15:0] led
+);
+	
+	reg [31:0] count;
+
+	always@(posedge clk) begin
+		if(reset == 1) begin
+			led <= 16'd1;
+			count <= 32'd0; end
+		else begin
+			if (count == 32'd0)
+				led <= {led[14:0], led[15]};
+			count <= (count >= 5000 ? 32'd0 : count + 1);
+			end
+	end
+endmodule
