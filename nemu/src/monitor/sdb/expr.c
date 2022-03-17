9e4bfb2dcb3ea7f6fb37d2f6a2c6a62528fa714c
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-17 15:09:01 +0800

    >  compile NEMU ysyx_22040386 程钇力 Linux cyl-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:09:01 up  1:34,  3 users,  load average: 0.01, 0.05, 0.05

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -192,8 +192,15 @@ uint32_t eval(int p, int q) {
 	}
 	else  {
 		int op = find_op(p,q);
-		uint32_t val1 = eval(p, op-1);
-		uint32_t val2 = eval(op+1, q);
+		uint32_t val1,val2 = 1;
+		if (tokens[op].type == TK_NEGATIVE) {
+			val2 = eval(op+1, q);
+			val1 = eval(op+1, q);
+		}
+		else {
+			val1 = eval(p, op-1);
+			val2 = eval(op+1, q);
+		}
 
 		switch (tokens[op].type) {
 			case '+': return (val1 + val2);
