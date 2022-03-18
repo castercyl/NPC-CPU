16d4e5f3342a0ba0d25ad4167c70f8d3286538ce
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-18 18:44:46 +0800

    >  compile NEMU ysyx_22040386 程钇力 Linux cyl-virtual-machine 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:44:46 up  3:15,  3 users,  load average: 0.24, 0.27, 0.16

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -222,7 +222,7 @@ uint32_t eval(int p, int q) {
 			case TK_NEQ: return (val1 != val2);
 			case TK_AND: return (val1 && val2);
 			case DEREF: return vaddr_read(val2, 4);
-			case TK_EQ: return val1 == val2;
+			case TK_EQ: return (val1 == val2);
 			default : assert(0);
 		}
 	}
